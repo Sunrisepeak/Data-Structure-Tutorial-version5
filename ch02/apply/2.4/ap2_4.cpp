e53d3f0afee7c7854dc1b14c20f4db1df69adf97
Your Name <you@example.com>
2019-04-17 23:20:38 +0800

    complete Table join

--- ch02/apply/2.4/ap2_4.cpp
+++ ch02/apply/2.4/ap2_4.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 int main()
 {
-	HNode *h1;
+	HNode *h1, *h2, *h;
+	cout << "Table1:\n";
 	CreateTable(h1);
+	cout << "Table2:\n";
+	CreateTable(h2);
+
 	DispTable(h1);
+	cout << endl;
+	DispTable(h2);
+
+	LinkTable(h1, h2, h);
+	cout << "link of Table result:" << endl;
+	DispTable(h);
+	DestroyTable(h1);
+	DestroyTable(h2);
+	DestroyTable(h);
 	return 0;
 }
--- ch02/apply/2.4/njoin.cpp
+++ ch02/apply/2.4/njoin.cpp
@@ -38,5 +38,54 @@ void DispTable(HList *h)
 	}
 }
 
+void DestroyTable(HList * &h)
+{
+	RList *pre = h->next, *p = pre->next;
+	while(p != NULL)
+	{
+		free(pre);
+		pre = p;
+		p = p->next;
+	}
+	free(pre);
+	free(h);
+}
+
+void LinkTable(HList * h1, HList *h2, HList * &h)
+{
+	RList *p = h1->next, *q, *s, *r;
+	std::cout << "link field(First Second):";
+	int i, j, k;
+	scanf("%d%d", &i, &j);
+	h = (HList *)malloc(sizeof(HList));
+	h->Row = 0;
+	h->Col = h1->Col + h2->Col;
+	h->next = NULL;
+	while(p != NULL)
+	{
+		q = h2->next;
+		while(q != NULL)
+		{
+			if(p->data[i - 1] == q->data[j - 1])
+			{
+				s = (RList *)malloc(sizeof(RList));
+				for(k = 0; k < h1->Col; k++)
+					s->data[k] = p->data[k];
+				for(k = 0; k < h2->Col; k++)
+					s->data[h1->Col + k] = q->data[k];
+				if(h->next == NULL)
+					h->next = s;
+				else
+					r->next = s;
+				r = s;
+				h->Row++;
+			}
+			q = q->next;
+		}
+		p = p->next;
+	}
+	r->next = NULL;
+}
+
 
 
